18238bf6865ff915a65b16d0d5aa1a94183eb207
tomenko <tomenko.sl@gmail.com>
2021-02-15 00:16:56 +0200

    работа над ошибками

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles.css';
-import {themeSwitch, valueThem, chekedSwitch, btnSwitch } from './modul-js/theme-switch';
+import {themeSwitch, valueThem, btnSwitch } from './modul-js/theme-switch';
 /* import {valueThem} from './modul-js/theme'; */
-console.log(btnSwitch);
-console.log(valueThem);
 console.log(themeSwitch);
+console.log(valueThem);
+console.log(btnSwitch);
+
+
+
--- src/modul-js/theme-switch.js
+++ src/modul-js/theme-switch.js
@@ -7,14 +7,14 @@ const body = document.querySelector("body");
 /* export let setItemLs = localStorage.setItem("them", Theme.LIGHT) */
 /* export let valueThem = body.classList.add(Theme.LIGHT); */
 export let valueThem = body.classList.toggle(localStorage.getItem("them"));
-export let chekedSwitch =() => {if (valueThem === Theme.DARK) {themeSwitch.checked = true}};;
+/* export let chekedSwitch =() => {if (valueThem === Theme.DARK) {themeSwitch.checked = true}}; */
+if (valueThem === Theme.DARK) {themeSwitch.checked = true}
+
 
 const cb = () => {
     /* valueThem = body.classList.toggle(Theme.DARK); */
     /* setItemLs = localStorage.setItem("them", Theme.DARK); */
-   
-    chekedSwitch();
-
+    
 
     if (localStorage.getItem("them") === Theme.LIGHT) {
         localStorage.setItem("them", Theme.DARK)
